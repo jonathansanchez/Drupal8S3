6671ba4e0c1035f65a52d3e70a5dbee9d2ef1076
Jonathan <jsanchezorellana@gmail.com>
2017-08-25 17:20:06 -0300

    Added status to Author

--- author/src/Domain/Author/Author.php
+++ author/src/Domain/Author/Author.php
@@ -8,11 +8,15 @@ class Author
     private $name;
     /** @var  AuthorImageProfile $imageProfile */
     private $imageProfile;
+    private $status;
+    private $createdAt;
 
     public function __construct(AuthorName $anAuthorName, AuthorImageProfile $anImageProfile)
     {
         $this->setName($anAuthorName);
         $this->setImageProfile($anImageProfile);
+        $this->deactivate();
+        $this->createdAt(new \DateTimeImmutable());
     }
 
     private function setName($anAuthorName)
@@ -33,5 +37,24 @@ class Author
     public function imageProfile()
     {
         return $this->imageProfile;
+    }
+
+    private function createdAt(\DateTimeImmutable $aDate)
+    {
+        $this->createdAt = $aDate;
+    }
+
+    private function setStatus(Status $anAuthorStatus) {
+        $this->status = $anAuthorStatus;
+    }
+
+    public function deactivate() {
+        $this->setStatus(
+            Status::deactivate()
+        );
+    }
+
+    public function activate() {
+        return $this->status->equalsTo(Status::active());
     }
 }
